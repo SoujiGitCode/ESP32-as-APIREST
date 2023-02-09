895c0c82164b01219f394e1eeda0acd42a2bd8d6
SoujiGitCode <dev.reinaldo.cardenas@gmail.com>
2023-02-09 15:18:47 +0100

    base json websocket

--- arduino.ino
+++ arduino.ino
@@ -1,108 +1,83 @@
-#include <WiFi.h>                                     // needed to connect to WiFi
-#include <WebServer.h>                                // needed to create a simple webserver (make sure tools -> board is set to ESP32, otherwise you will get a "WebServer.h: No such file or directory" error)
-#include <WebSocketsServer.h>                         // needed for instant communication between client and server through Websockets
-#include <ArduinoJson.h>                              // needed for JSON encapsulation (send multiple variables with one string)
+#include <WiFi.h>
+#include <ESPAsyncWebServer.h>
+#include <ArduinoJson.h>
+#include <WebSocketsServer.h>
 
-// SSID and password of Wifi connection:
-const char* ssid = "ESP32";
-const char* password = "password";
+const char *ssid = "MyAP";
+const char *password = "password";
 
-// Configure IP addresses of the local access point
-IPAddress local_IP(192,168,1,22);
-IPAddress gateway(192,168,1,5);
-IPAddress subnet(255,255,255,0);
+AsyncWebServer server(80);
+WebSocketsServer webSocket = WebSocketsServer(81);
 
+void handleRoot(AsyncWebServerRequest *request) {
+  StaticJsonDocument<200> doc;
+  doc["message"] = "bienvenido a esp32";
+  String output;
+  serializeJson(doc, output);
+  request->send(200, "application/json", output);
+}
 
-// The String below "webpage" contains the complete HTML code that is sent to the client whenever someone connects to the webserver
-String webpage = "<!DOCTYPE html><html><head><title>Page Title</title></head><body style='background-color: #EEEEEE;'><span style='color: #003366;'><h1>Lets generate a random number</h1><p>The first random number is: <span id='rand1'>-</span></p><p>The second random number is: <span id='rand2'>-</span></p><p><button type='button' id='BTN_SEND_BACK'>Send info to ESP32</button></p></span></body><script> var Socket; document.getElementById('BTN_SEND_BACK').addEventListener('click', button_send_back); function init() { Socket = new WebSocket('ws://' + window.location.hostname + ':81/'); Socket.onmessage = function(event) { processCommand(event); }; } function button_send_back() { var msg = {brand: 'Gibson',type: 'Les Paul Studio',year: 2010,color: 'white'};Socket.send(JSON.stringify(msg)); } function processCommand(event) {var obj = JSON.parse(event.data);document.getElementById('rand1').innerHTML = obj.rand1;document.getElementById('rand2').innerHTML = obj.rand2; console.log(obj.rand1);console.log(obj.rand2); } window.onload = function(event) { init(); }</script></html>";
+void handleApi(AsyncWebServerRequest *request) {
+  StaticJsonDocument<200> doc;
+  doc["num1"] = random(100);
+  doc["num2"] = random(100);
+  doc["num3"] = random(100);
+  String output;
+  serializeJson(doc, output);
+  request->send(200, "application/json", output);
+}
 
-// The JSON library uses static memory, so this will need to be allocated:
-// -> in the video I used global variables for "doc_tx" and "doc_rx", however, I now changed this in the code to local variables instead "doc" -> Arduino documentation recomends to use local containers instead of global to prevent data corruption
+void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t length) {
+  switch(type) {
+    case WStype_DISCONNECTED:
+      Serial.printf("[%u] Disconnected!\n", num);
+      break;
+    case WStype_CONNECTED: {
+        Serial.printf("[%u] Connected from url: %s\n", num, payload);
+        // send message to client
+        webSocket.sendTXT(num, "Connected");
+      }
+      break;
+    case WStype_TEXT:
+      Serial.printf("[%u] Message: %s\n", num, payload);
+      break;
+    case WStype_BIN:
+      Serial.printf("[%u] Message [BINARY]: %u bytes\n", num, length);
+      break;
+  }
+}
 
-// We want to periodically send values to the clients, so we need to define an "interval" and remember the last time we sent data to the client (with "previousMillis")
-int interval = 1000;                                  // send data to the client every 1000ms -> 1s
-unsigned long previousMillis = 0;                     // we use the "millis()" command for time reference and this will output an unsigned long
-
-// Initialization of webserver and websocket
-WebServer server(80);                                 // the server uses port 80 (standard port for websites
-WebSocketsServer webSocket = WebSocketsServer(81);    // the websocket uses port 81 (standard port for websockets
 
 void setup() {
-  Serial.begin(115200);                               // setting up a
- 
-  Serial.print("Setting up Access Point ... ");
-  Serial.println(WiFi.softAPConfig(local_IP, gateway, subnet) ? "Ready" : "Failed!");
+  WiFi.softAP(ssid, password);
+  server.on("/", HTTP_GET, handleRoot);
+  server.on("/api", HTTP_GET, handleApi);
+  server.begin();
 
-  Serial.print("Starting Access Point ... ");
-  Serial.println(WiFi.softAP(ssid, password) ? "Ready" : "Failed!");
+  webSocket.begin();
+  webSocket.onEvent(webSocketEvent);
 
-  Serial.print("IP address = ");
-  Serial.println(WiFi.softAPIP());
-
-  server.begin();                                     // start server
-  
-  server.on("/", []() {                               // define here wat the webserver needs to do
-    server.send(200, "text/html", jsonWeb);           //    -> it needs to send out the HTML string "webpage" to the client
-  });
-  
-  server.begin();                                     // start server
-  
-  webSocket.begin();                                  // start websocket
-  webSocket.onEvent(webSocketEvent);                  // define a callback function -> what does the ESP32 need to do when an event from the websocket is received? -> run function "webSocketEvent()"
+  Serial.println("Webserver Started!");
 }
 
 void loop() {
-  server.handleClient();                              // Needed for the webserver to handle all clients
-  webSocket.loop();                                   // Update function for the webSockets 
-  
-  unsigned long now = millis();                       // read out the current "time" ("millis()" gives the time in ms since the Arduino started)
-  if ((unsigned long)(now - previousMillis) > interval) { // check if "interval" ms has passed since last time the clients were updated
-    
-    String jsonString = "";                           // create a JSON string for sending data to the client
-    StaticJsonDocument<200> doc;                      // create a JSON container
-    JsonObject object = doc.to<JsonObject>();         // create a JSON Object
-    object["a1"] = random(100);                    // write data into the JSON object -> I used "rand1" and "rand2" here, but you can use anything else
-    object["a2"] = random(100);
-    object["a3"] = random(100);                    // write data into the JSON object -> I used "rand1" and "rand2" here, but you can use anything else
-    serializeJson(doc, jsonString);                   // convert JSON object to string
-    Serial.println(jsonString);                       // print JSON string to console for debug purposes (you can comment this out)
-    webSocket.broadcastTXT(jsonString);               // send JSON string to clients
-    
-    previousMillis = now;                             // reset previousMillis
+  webSocket.loop();
+
+  static unsigned long last_time = 0;
+  if (millis() - last_time >= 1000) {
+    last_time = millis();
+    int number1 = random(50,100);
+    int number2 = random(50,100);
+    int number3 = random(50,100);
+    int number4 = random(50,100);
+
+    String message = "{\"number1\": " + String(number1) + ",\"number2\": " + String(number2) + ",\"number3\": " + String(number3) + "}";
+    String longMessage= "{\"sections\":{\"a\":{\"a1\":"+String(number1)+",\"a2\":"+String(number2)+",\"a3\":"+String(number3)+",\"a4\":"+String(number4)+"},\"b\":{\"b1\":"+String(number1)+",\"b2\":"+String(number2)+",\"b3\":"+String(number3)+",\"b4\":"+String(number4)+"},\"c\":{\"c1\":"+String(number1)+",\"c2\":"+String(number2)+",\"c3\":"+String(number3)+",\"c4\":"+String(number4)+"}}}";
+
+
+    webSocket.broadcastTXT(longMessage);
   }
 }
 
-void webSocketEvent(byte num, WStype_t type, uint8_t * payload, size_t length) {      // the parameters of this callback function are always the same -> num: id of the client who send the event, type: type of message, payload: actual data sent and length: length of payload
-  switch (type) {                                     // switch on the type of information sent
-    case WStype_DISCONNECTED:                         // if a client is disconnected, then type == WStype_DISCONNECTED
-      Serial.println("Client " + String(num) + " disconnected");
-      break;
-    case WStype_CONNECTED:                            // if a client is connected, then type == WStype_CONNECTED
-      Serial.println("Client " + String(num) + " connected");
-      // optionally you can add code here what to do when connected
-      break;
-    case WStype_TEXT:                                 // if a client has sent data, then type == WStype_TEXT
-      // try to decipher the JSON string received
-      StaticJsonDocument<200> doc;                    // create a JSON container
-      DeserializationError error = deserializeJson(doc, payload);
-      if (error) {
-        Serial.print(F("deserializeJson() failed: "));
-        Serial.println(error.f_str());
-        return;
-      }
-      else {
-        // JSON string was received correctly, so information can be retrieved:
-        const char* g_brand = doc["brand"];
-        const char* g_type = doc["type"];
-        const int g_year = doc["year"];
-        const char* g_color = doc["color"];
-        Serial.println("Received guitar info from user: " + String(num));
-        Serial.println("Brand: " + String(g_brand));
-        Serial.println("Type: " + String(g_type));
-        Serial.println("Year: " + String(g_year));
-        Serial.println("Color: " + String(g_color));
-      }
-      Serial.println("");
-      break;
-  }
-}
+
+
---
+++ reame.md
@@ -0,0 +1,9 @@
+Steps:
+
+1 - Install ESPAsyncWebServer Library from:
+https://github.com/me-no-dev/ESPAsyncWebServer
+
+2 - Install AsyncTCP Library from:
+https://github.com/me-no-dev/AsyncTCP
+
+3 -
